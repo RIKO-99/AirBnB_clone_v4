6aa54ff32f353b511fcb7d437c0f2268bb322ff1
Manuel254 <Immanuelkituku@gmail.com>
2023-05-05 09:28:47 +0300

    Fix status bg color

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,11 +20,15 @@ $(function () {
     $('div.amenities h4').text(checkedStr);
   });
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-    if (data.status == 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
+  $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      dataType: 'json',
+      success:  function(data) {
+        if (data.status == 'OK') {
+          $('div#api_status').addClass('available');
+        } else {
+          $('div#api_status').removeClass('available');
+        }
+      }
   });
 });
