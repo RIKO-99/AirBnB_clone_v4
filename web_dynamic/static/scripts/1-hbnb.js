79f12ba3f03b13b793c41b99e02a2706de6f6603
Manuel254 <Immanuelkituku@gmail.com>
2023-05-05 13:58:28 +0300

    Add places to be fetched by jQuery instead of flask

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,19 +2,19 @@ $(function () {
   let checked = [];
   let checkedStr = '';
 
-  $('input[type="checkbox"]').on('change', function() {
-    let id = $(this).data('id');
-    let name = $(this).data('name');
+  $('input[type="checkbox"]').on('change', function () {
+    const id = $(this).data('id');
+    const name = $(this).data('name');
 
     if ($(this).is(':checked')) {
-      checked.push({'id': id,'name': name});
+      checked.push({ id: id, name: name });
     } else {
       checked = $.grep(checked, (item) => {
         return item.id != id;
       });
     }
 
-    checkedStr = checked.map(function(item) {
+    checkedStr = checked.map(function (item) {
       return item.name;
     }).join(', ');
     $('div.amenities h4').text(checkedStr);
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,33 +2,33 @@ $(function () {
   let checked = [];
   let checkedStr = '';
 
-  $('input[type="checkbox"]').on('change', function() {
-    let id = $(this).data('id');
-    let name = $(this).data('name');
+  $('input[type="checkbox"]').on('change', function () {
+    const id = $(this).data('id');
+    const name = $(this).data('name');
 
     if ($(this).is(':checked')) {
-      checked.push({'id': id,'name': name});
+      checked.push({ id: id, name: name });
     } else {
       checked = $.grep(checked, (item) => {
         return item.id != id;
       });
     }
 
-    checkedStr = checked.map(function(item) {
+    checkedStr = checked.map(function (item) {
       return item.name;
     }).join(', ');
     $('div.amenities h4').text(checkedStr);
   });
 
   $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/status/',
-      dataType: 'json',
-      success:  function(data) {
-        if (data.status == 'OK') {
-          $('div#api_status').addClass('available');
-        } else {
-          $('div#api_status').removeClass('available');
-        }
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status == 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
       }
+    }
   });
 });
